61bd8a4180a5a1c5f64db22c6d246d1b5e04d33f
Eugene Weiss <eugene.a.weiss@gmail.com>
2023-12-05 19:43:26 +0000

    Set up the helper code as a local package

---
+++ Notebooks/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+	name='utils',
+	version='1.0',
+	description='YOLO model helper package',
+	url='https://github.com/aihgii/YOLO',
+	author='Eugene Weiss',
+	install_requires=[],
+	author_email='eugene.a.weiss@gmail.com',
+	packages=setuptools.find_packages(),
+	zip_safe=False
+)
